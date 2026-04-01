e7e9a9357e86a4abe5cf6e83b24109815de7faf4
Ranim Rtimi <rtimiranim80@gmail.com>
2026-04-01 02:10:53 +0100

    feat(ui): Ajout des bilans mensuels/annuels et refonte de la page de login

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Repository\SalesLogRepository;
 use App\Repository\PurchaseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,10 +13,49 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractController
 {
     #[Route('/', name: 'admin_dashboard')]
-    public function index(SalesLogRepository $salesLogRepository, PurchaseRepository $purchaseRepository): Response
+    public function index(Request $request, SalesLogRepository $salesLogRepository, PurchaseRepository $purchaseRepository): Response
     {
-        $salesLogs = $salesLogRepository->findBy([], ['date' => 'DESC']);
-        $purchases = $purchaseRepository->findBy([], ['date' => 'DESC']);
+        $filterDateStr = $request->query->get('date', 'today'); // Default to 'today'
+        
+        $salesQuery = $salesLogRepository->createQueryBuilder('s')->orderBy('s.date', 'DESC');
+        $purchaseQuery = $purchaseRepository->createQueryBuilder('p')->orderBy('p.date', 'DESC');
+
+        if ($filterDateStr && $filterDateStr !== 'all') {
+            try {
+                if ($filterDateStr === 'today') {
+                    $filterDate = new \DateTime('today');
+                    $salesQuery->andWhere('s.date = :date')->setParameter('date', $filterDate->format('Y-m-d'));
+                    $purchaseQuery->andWhere('p.date = :date')->setParameter('date', $filterDate->format('Y-m-d'));
+                } elseif ($filterDateStr === 'month') {
+                    $start = new \DateTime('first day of this month');
+                    $end = new \DateTime('first day of next month');
+                    $salesQuery->andWhere('s.date >= :start AND s.date < :end')
+                               ->setParameter('start', $start->format('Y-m-d'))
+                               ->setParameter('end', $end->format('Y-m-d'));
+                    $purchaseQuery->andWhere('p.date >= :start AND p.date < :end')
+                               ->setParameter('start', $start->format('Y-m-d'))
+                               ->setParameter('end', $end->format('Y-m-d'));
+                } elseif ($filterDateStr === 'year') {
+                    $start = new \DateTime('first day of January this year');
+                    $end = new \DateTime('first day of January next year');
+                    $salesQuery->andWhere('s.date >= :start AND s.date < :end')
+                               ->setParameter('start', $start->format('Y-m-d'))
+                               ->setParameter('end', $end->format('Y-m-d'));
+                    $purchaseQuery->andWhere('p.date >= :start AND p.date < :end')
+                               ->setParameter('start', $start->format('Y-m-d'))
+                               ->setParameter('end', $end->format('Y-m-d'));
+                } else {
+                    $filterDate = new \DateTime($filterDateStr);
+                    $salesQuery->andWhere('s.date = :date')->setParameter('date', $filterDate->format('Y-m-d'));
+                    $purchaseQuery->andWhere('p.date = :date')->setParameter('date', $filterDate->format('Y-m-d'));
+                }
+            } catch (\Exception $e) {
+                // Ignore invalid
+            }
+        }
+
+        $salesLogs = $salesQuery->getQuery()->getResult();
+        $purchases = $purchaseQuery->getQuery()->getResult();
         
         $totalCalculated = 0;
         $totalAnnounced = 0;
@@ -26,7 +66,6 @@ class DashboardController extends AbstractController
             $totalAnnounced += $log->getAnnouncedTotal();
 
             $diff = $log->getCalculatedTotal() - $log->getAnnouncedTotal();
-            // Store logs where worker announced total differs from formula based calculation
             if (abs($diff) > 0.01) {
                 $discrepancies[] = [
                     'log' => $log,
@@ -48,6 +87,7 @@ class DashboardController extends AbstractController
             'netProfit' => $totalAnnounced - $totalExpenses,
             'discrepancies' => $discrepancies,
             'recentLogs' => array_slice($salesLogs, 0, 10),
+            'currentDateFilter' => $filterDateStr,
         ]);
     }
 }
